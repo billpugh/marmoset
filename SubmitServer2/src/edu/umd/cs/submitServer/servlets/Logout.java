9e4a241464e07208de50879934c62953a96d88f1
Bill Pugh <pugh@cs.umd.edu>
2019-10-22 13:21:48 -0400

    Force logout from CAS

--- SubmitServer2/src/edu/umd/cs/submitServer/servlets/Logout.java
+++ SubmitServer2/src/edu/umd/cs/submitServer/servlets/Logout.java
@@ -33,33 +33,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import edu.umd.cs.submitServer.UserSession;
+
 /**
  * @author pugh
  * 
  */
 public class Logout extends SubmitServerServlet {
 
-	/**
-	 * The doGet method of the servlet. <br>
-	 * 
-	 * This method is called when a form has its tag value method equals to get.
-	 * 
-	 * @param request
-	 *            the request send by the client to the server
-	 * @param response
-	 *            the response send by the server to the client
-	 * @throws ServletException
-	 *             if an error occurred
-	 * @throws IOException
-	 *             if an error occurred
-	 */
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
 		HttpSession session = request.getSession();
+	  UserSession userSession = session == null ? null : (UserSession) session.getAttribute(USER_SESSION);
+
 		session.invalidate();
-		response.sendRedirect(request.getContextPath() + "/index.jsp");
+		if (userSession != null && userSession.isCasLogin())
+	    response.sendRedirect("https://login.umd.edu/cas/logout");
+		else
+		  response.sendRedirect(request.getContextPath() + "/index.jsp");
 	}
 
 }
